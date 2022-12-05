149c903a460e4419b5e261e1c3c11115eff55e31
Lertos <deetasticgaming@gmail.com>
2022-12-05 11:52:04 -0400

    Making numbers formatted with commas; add new lines and tabs

--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -28,13 +28,31 @@ public class PlayerStats {
             }
 
             //The replace is because some headers have the word "rank" in them, but we already specify it
-            sb.append(columns[0].replace(" - Rank", "") + " = ");
+            sb.append(columns[0].replace(" - Rank", "") + "\n\t");
 
-            for (int i=0; i<descriptors.size(); i++)
-                sb.append(descriptors.get(i) + columns[i+1] + " ");
+            if (isUnranked(columns))
+                sb.append("Unranked");
+            else {
+                int descSize = descriptors.size();
 
+                for (int i = 0; i < descSize; i++) {
+                    if (i == descSize-1)
+                        sb.append(descriptors.get(i) + String.format("%,d", Integer.parseInt(columns[i + 1])));
+                    else
+                        sb.append(descriptors.get(i) + String.format("%,d", Integer.parseInt(columns[i + 1])) + ", ");
+                }
+            }
             System.out.println(sb.toString());
         }
         System.out.println();
     }
+
+    private boolean isUnranked(String[] columns) {
+        for (String column : columns) {
+            if (column.equalsIgnoreCase("-1"))
+                return true;
+        }
+        return false;
+    }
+
 }
